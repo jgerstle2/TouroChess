5b6f6b47c92c8bae665cc49c91703638786f8766
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-09-12 04:37:47 -0400

    changed the code to set up the board in 2 steps according to feedback

--- src/main/java/touro/chess/Board.java
+++ src/main/java/touro/chess/Board.java
@@ -8,7 +8,23 @@ public class Board {
     private static final int ROWS = 8;
     private Square[][] squares = new Square[COLUMNS][ROWS];
 
+    /**
+     * Creates an empty board
+     */
     public Board()
+    {
+        for(int column = 0; column < COLUMNS; column++) {
+            for (int row = 0; row < ROWS; row++) {
+                squares[column][row] = new Square(new Location(row, column));
+            }
+        }
+    }
+
+    /**
+     * Sets up the board at the beginning of the game
+     */
+
+    public void setUpBoard()
     {
         for(int column = 0; column < COLUMNS; column++)
         {
@@ -18,35 +34,32 @@ public class Board {
                 PieceColor color = row < 2 ? PieceColor.White : PieceColor.Black;
                 if (row == 1 || row == 6) //pawn
                 {
-                    //squares[column][row] = new Square(location, new PawnPiece(location, color));
+                    //squares[column][row].setPiece(new PawnPiece(location, color));
                 }
                 else  if (row == 0 || row == 7)
                 {
                     if (column == 0 || column == 7) //rook
                     {
-                        //squares[column][row] = new Square(location, new RookPiece(location, color));
+                        //squares[column][row].setPiece(new RookPiece(location, color));
                     }
                     else if (column == 1 || column == 6) //knight
                     {
-                        //squares[column][row] = new Square(location, new KnightPiece(location, color));
+                        //squares[column][row].setPiece(new KnightPiece(location, color));
                     }
                     else if (column == 2 || column == 5) //bishop
                     {
-                        //squares[column][row] = new Square(location, new BishopPiece(location, color));
+                        //squares[column][row].setPiece(new BishopPiece(location, color));
                     }
                     else if(column == 3) //king
                     {
-                        //squares[column][row] = new Square(location, new KingPiece(location, color));
+                        //squares[column][row].setPiece(new KingPiece(location, color));
                     }
                     else if (column == 4) //queen
                     {
-                        //squares[column][row] = new Square(location, new QueenPiece(location, color));
+                        //squares[column][row].setPiece(new QueenPiece(location, color));
                     }
                 }
-                else
-                {
-                    squares[column][row] = new Square(location);
-                }
+
             }
         }
     }
