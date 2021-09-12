1d21594c3b9fdfdd91a87cc73b0ad2e51ab64612
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-09-12 17:05:55 -0400

    Created chess board with pieces in starting positions

---
+++ src/main/java/touro/chess/ChessController.java
@@ -0,0 +1,18 @@
+package touro.chess;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+
+public class ChessController {
+
+    @FXML
+    ArrayList<Label> lightLabels, darkLabels;
+
+    public void initialize() {
+
+    }
+
+
+}
