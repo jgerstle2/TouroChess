da6bb3459e886860dd3b0e9ded78b769df12437f
ElianaDG <58051386+ElianaDG@users.noreply.github.com>
2021-09-30 17:45:16 -0400

    shortened isLegal method and working on testing

--- src/main/java/touro/chess/Board.java
+++ src/main/java/touro/chess/Board.java
@@ -9,30 +9,34 @@ public class Board {
     /**
      * Creates an empty board
      */
-    public Board()
-    {
-        for(int column = 0; column < COLUMNS; column++) {
+    public Board() {
+        for (int column = 0; column < COLUMNS; column++) {
             for (int row = 0; row < ROWS; row++) {
                 squares[column][row] = new Square(new Location(row, column));
             }
         }
     }
 
-    public Square getSquare(Location location){
+    public Square getSquare(Location location) {
         return squares[location.getRow()][location.getColumn()];
     }
 
-    public boolean isOnBoard(Move move){
+    public boolean isOnBoard(Move move) {
         return isOnBoard(move.getFrom()) && isOnBoard(move.getTo());
     }
 
-    public boolean isOnBoard(Location location){
+    public boolean isOnBoard(Location location) {
         return location.getRow() > -1 && location.getRow() < 8
                 && location.getColumn() > 0 && location.getColumn() < 8;
     }
 
+    public void setPiece(Location location, AbstractPiece piece) {
+
+    }
+
     /**
      * Verify that a Move is legal on this Board.
+     *
      * @param move
      * @return true if the Move is legal, otherwise false.
      */
@@ -42,102 +46,39 @@ public class Board {
         int destinationRow = move.getTo().getRow();
         int destinationColumn = move.getTo().getColumn();
 
-        if (!isOnBoard(move))
-        {
+        if (!isOnBoard(move)) {
             return false;
         }
 
-        Square currentSquare = getSquare(move.getFrom());
+        Square currentSquare = getSquare(move.getFrom());   //how else can i get the location if it doesnt extend AbstractPiece?
         Square destinationSquare = getSquare(move.getTo());
         AbstractPiece currentPiece = currentSquare.getPiece();
         AbstractPiece destinationPiece = destinationSquare.getPiece();
 
-        if (currentPiece == null){
+        if (currentPiece == null) {
             return false;
-        }
-        else if(move.getFrom() == move.getTo()){
+        } else if (move.getFrom() == move.getTo()) {
             return false;
-        }
-        else if (destinationPiece != null) {
+        } else if (destinationPiece != null) {
             if (destinationSquare.hasPiece() && destinationPiece.getColor() == currentPiece.getColor()) {
                 return false;
             }
-        }
-        else {   //if destination is empty or contains opposing color piece
+        } else {   //if destination is empty or contains opposing color piece
             if (move.isJump()) {
                 return true;
-            }
-            else if (currentColumn == destinationColumn) { //vertical
-                if (currentRow < destinationRow) { //forward
-                    for (int row = (currentRow + 1); row < destinationRow; row++) { //increment row, keep column
-                        if(squares[row][currentColumn].hasPiece()){
-                            return false;
-                        }
+            } else {
+                while (currentRow != destinationRow && currentColumn != destinationColumn) {
+                    if (currentRow < destinationRow) {
+                        currentRow++;
+                    } else {
+                        currentRow--;
                     }
-                }
-                else{ //backwards
-                    for (int row = (currentRow - 1); row > destinationRow; row--) { //decrement row, keep column
-                        if(squares[row][currentColumn].hasPiece()){
-                            return false;
-                        }
+                    if (currentColumn < destinationColumn) {
+                        currentColumn++;
+                    } else {
+                        currentColumn--;
                     }
-                }
-                return true;
-            }
-            else if (currentRow == destinationRow) { //horizontal
-                if (currentColumn < destinationColumn) { //right
-                    for (int col = (currentColumn + 1); col < destinationColumn; col++) { //increment column, keep row
-                        if(squares[currentRow][col].hasPiece()){
-                            return false;
-                        }
-                    }
-                }
-                else { //left
-                    for (int col = (currentColumn - 1); col > destinationColumn; col--) { //decrement column, keep row
-                        if(squares[currentRow][col].hasPiece()){
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            }
-            else if ((currentColumn - destinationColumn) == (currentRow - destinationRow)) { //diagonal
-                int row = currentRow;
-                if (currentRow < destinationRow) { //forward
-                    if (currentColumn < destinationColumn) { //right
-                        for (int col = (currentColumn + 1); col < destinationColumn; col++) { //increment row, increment column
-                            row++;
-                            if(squares[row][col].hasPiece()){
-                                return false;
-                            }
-                        }
-                    }
-                    else { //left
-                        for (int col = (currentColumn - 1); col > destinationColumn; col--) { //increment row, decrement column
-                            row++;
-                            if(squares[row][col].hasPiece()){
-                                return false;
-                            }
-                        }
-                    }
-                }
-                else { //backward
-                    if (currentColumn < destinationColumn) { //right
-                        for (int col = (currentColumn + 1); col < destinationColumn; col++) { //decrement row, increment column
-                            row--;
-                            if(squares[row][col].hasPiece()){
-                                return false;
-                            }
-                        }
-                    }
-                    else { //left
-                        for (int col = (currentColumn - 1); col > destinationColumn; col--) { //decrement row, decrement column
-                            row--;
-                            if(squares[row][col].hasPiece()){
-                                return false;
-                            }
-                        }
-                    }
+                    //if the new location has a piece, return false
                 }
                 return true;
             }
@@ -145,3 +86,81 @@ public class Board {
         return false;
     }
 }
+//            else if (currentColumn == destinationColumn) { //vertical
+//                if (currentRow < destinationRow) { //forward
+//                    for (int row = (currentRow + 1); row < destinationRow; row++) { //increment row, keep column
+//                        if(squares[row][currentColumn].hasPiece()){
+//                            return false;
+//                        }
+//                    }
+//                }
+//                else{ //backwards
+//                    for (int row = (currentRow - 1); row > destinationRow; row--) { //decrement row, keep column
+//                        if(squares[row][currentColumn].hasPiece()){
+//                            return false;
+//                        }
+//                    }
+//                }
+//                return true;
+//            }
+//            else if (currentRow == destinationRow) { //horizontal
+//                if (currentColumn < destinationColumn) { //right
+//                    for (int col = (currentColumn + 1); col < destinationColumn; col++) { //increment column, keep row
+//                        if(squares[currentRow][col].hasPiece()){
+//                            return false;
+//                        }
+//                    }
+//                }
+//                else { //left
+//                    for (int col = (currentColumn - 1); col > destinationColumn; col--) { //decrement column, keep row
+//                        if(squares[currentRow][col].hasPiece()){
+//                            return false;
+//                        }
+//                    }
+//                }
+//                return true;
+//            }
+//            else if ((currentColumn - destinationColumn) == (currentRow - destinationRow)) { //diagonal
+//                int row = currentRow;
+//                if (currentRow < destinationRow) { //forward
+//                    if (currentColumn < destinationColumn) { //right
+//                        for (int col = (currentColumn + 1); col < destinationColumn; col++) { //increment row, increment column
+//                            row++;
+//                            if(squares[row][col].hasPiece()){
+//                                return false;
+//                            }
+//                        }
+//                    }
+//                    else { //left
+//                        for (int col = (currentColumn - 1); col > destinationColumn; col--) { //increment row, decrement column
+//                            row++;
+//                            if(squares[row][col].hasPiece()){
+//                                return false;
+//                            }
+//                        }
+//                    }
+//                }
+//                else { //backward
+//                    if (currentColumn < destinationColumn) { //right
+//                        for (int col = (currentColumn + 1); col < destinationColumn; col++) { //decrement row, increment column
+//                            row--;
+//                            if(squares[row][col].hasPiece()){
+//                                return false;
+//                            }
+//                        }
+//                    }
+//                    else { //left
+//                        for (int col = (currentColumn - 1); col > destinationColumn; col--) { //decrement row, decrement column
+//                            row--;
+//                            if(squares[row][col].hasPiece()){
+//                                return false;
+//                            }
+//                        }
+//                    }
+//                }
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
--- src/test/java/touro/chess/BoardTest.java
+++ src/test/java/touro/chess/BoardTest.java
@@ -1,41 +1,27 @@
 package touro.chess;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
 
 public class BoardTest {
 
-    private final Square[][] squares = new Square[8][8];
     private final Board board = new Board();
-
-    public void createSquares(){
-        for(int column = 0; column < 8; column++) {
-            for (int row = 0; row < 8; row++) {
-                squares[column][row] = new Square(new Location(row, column));
-            }
-        }
-    }
-
-    @BeforeClass
-    public static void beforeClass() {
-        com.sun.javafx.application.PlatformImpl.startup(() -> {
-        });
-    }
 
     @Test
     public void isLegal_validVerticalMove(){
         //given
-        createSquares();
-
-        //when
+        AbstractPiece currentPiece = mock(AbstractPiece.class);
+        AbstractPiece destinationPiece = mock(AbstractPiece.class);
         Location currentLocation = new Location(2,1);
         Location destinationLocation = new Location(4,1);
-        Square currentSquare = squares[2][1];
-        Square destinationSquare = squares[4][1];
+        board.setPiece(currentLocation, currentPiece);
+        board.setPiece(destinationLocation, destinationPiece);
         Move move = new Move(currentLocation,destinationLocation,false);
-        currentSquare.setPiece(new RookPiece(currentLocation, PieceColor.White));
-        destinationSquare.setPiece(new RookPiece(destinationLocation, PieceColor.Black));
+
+        //when
+        board.isLegal(move);
 
         //then
         Assert.assertTrue(board.isLegal(move));
@@ -44,16 +30,12 @@ public class BoardTest {
     @Test
     public void isLegal_invalidVerticalMove(){
         //given
-        createSquares();
-
-        //when
         Location currentLocation = new Location(2,1);
         Location destinationLocation = new Location(4,1);
-        Square currentSquare = squares[2][1];
-        Square destinationSquare = squares[4][1];
-        currentSquare.setPiece(new RookPiece(currentLocation, PieceColor.White));
-        destinationSquare.setPiece(new RookPiece(destinationLocation, PieceColor.White));
         Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
 
         //then
         Assert.assertFalse(board.isLegal(move));
@@ -62,16 +44,12 @@ public class BoardTest {
     @Test
     public void isLegal_validHorizontalMove(){
         //given
-        createSquares();
-
-        //when
         Location currentLocation = new Location(4,1);
         Location destinationLocation = new Location(4,6);
-        Square currentSquare = squares[4][1];
-        Square destinationSquare = squares[4][6];
-        currentSquare.setPiece(new RookPiece(currentLocation, PieceColor.White));
-        destinationSquare.setPiece(new RookPiece(destinationLocation, PieceColor.Black));
         Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
 
         //then
         Assert.assertTrue(board.isLegal(move));
@@ -80,19 +58,12 @@ public class BoardTest {
     @Test
     public void isLegal_invalidHorizontalMove(){
         //given
-        createSquares();
-
-        //when
         Location currentLocation = new Location(2,1);
         Location destinationLocation = new Location(2,5);
-        Location middleLocation = new Location(2,4);
-        Square currentSquare = squares[2][1];
-        Square destinationSquare = squares[2][5];
-        Square middleSquare = squares[2][4];
-        currentSquare.setPiece(new RookPiece(currentLocation, PieceColor.White));
-        middleSquare.setPiece(new BishopPiece(middleLocation, PieceColor.Black));
-        destinationSquare.setPiece(new RookPiece(destinationLocation, PieceColor.Black));
         Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
 
         //then
         Assert.assertFalse(board.isLegal(move));
@@ -101,16 +72,12 @@ public class BoardTest {
     @Test
     public void isLegal_validDiagonalMove(){
         //given
-        createSquares();
-
-        //when
         Location currentLocation = new Location(2,1);
         Location destinationLocation = new Location(4,3);
-        Square currentSquare = squares[2][1];
-        Square destinationSquare = squares[4][3];
-        currentSquare.setPiece(new QueenPiece(currentLocation, PieceColor.White));
-        destinationSquare.setPiece(new PawnPiece(destinationLocation, PieceColor.Black));
         Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
 
         //then
         Assert.assertTrue(board.isLegal(move));
@@ -119,16 +86,12 @@ public class BoardTest {
     @Test
     public void isLegal_invalidDiagonalMove(){
         //given
-        createSquares();
-
-        //when
         Location currentLocation = new Location(0,0);
         Location destinationLocation = new Location(7,7);
-        Square currentSquare = squares[0][0];
-        Square destinationSquare = squares[7][7];
-        currentSquare.setPiece(new QueenPiece(currentLocation, PieceColor.White));
-        destinationSquare.setPiece(new RookPiece(destinationLocation, PieceColor.White));
         Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
 
         //then
         Assert.assertFalse(board.isLegal(move));
@@ -137,14 +100,12 @@ public class BoardTest {
     @Test
     public void isLegal_validJumpMove(){
         //given
-        createSquares();
-
-        //when
         Location currentLocation = new Location(1,2);
         Location destinationLocation = new Location(3,1);
-        Square currentSquare = squares[1][2];
-        currentSquare.setPiece(new KnightPiece(currentLocation, PieceColor.White));
         Move move = new Move(currentLocation,destinationLocation,true);
+
+        //when
+        board.isLegal(move);
 
         //then
         Assert.assertTrue(board.isLegal(move));
@@ -153,16 +114,12 @@ public class BoardTest {
     @Test
     public void isLegal_invalidJumpMove(){
         //given
-        createSquares();
-
-        //when
         Location currentLocation = new Location(1,2);
         Location destinationLocation = new Location(2,4);
-        Square currentSquare = squares[1][2];
-        Square destinationSquare = squares[2][4];
-        currentSquare.setPiece(new KnightPiece(currentLocation, PieceColor.White));
-        destinationSquare.setPiece(new RookPiece(destinationLocation, PieceColor.White));
         Move move = new Move(currentLocation,destinationLocation,true);
+
+        //when
+        board.isLegal(move);
 
         //then
         Assert.assertFalse(board.isLegal(move));
@@ -171,26 +128,12 @@ public class BoardTest {
     @Test
     public void isLegal_invalidMoveOffBoard(){
         //given
-        createSquares();
-
-        //when
         Location currentLocation = new Location(12,1);
         Location destinationLocation = new Location(4,1);
         Move move = new Move(currentLocation,destinationLocation,false);
 
-        //then
-        Assert.assertFalse(board.isLegal(move));
-    }
-
-    @Test
-    public void isLegal_invalidMoveNullPiece(){
-        //given
-        createSquares();
-
         //when
-        Location currentLocation = new Location(2,1);
-        Location destinationLocation = new Location(2,5);
-        Move move = new Move(currentLocation,destinationLocation,false);
+        board.isLegal(move);
 
         //then
         Assert.assertFalse(board.isLegal(move));
@@ -199,12 +142,12 @@ public class BoardTest {
     @Test
     public void isLegal_invalidMoveSameSquare(){
         //given
-        createSquares();
-
-        //when
         Location currentLocation = new Location(4,1);
         Location destinationLocation = new Location(4,1);
         Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
 
         //then
         Assert.assertFalse(board.isLegal(move));
