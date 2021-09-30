cb867e9423ee8589e7f715490075c5b619fe88b8
jennygerstle@gmail.com <jennygerstle@gmail.com>
2021-09-30 09:59:16 -0400

    fixed the test

--- src/test/java/touro/chess/RookPieceTest.java
+++ src/test/java/touro/chess/RookPieceTest.java
@@ -3,6 +3,9 @@ package touro.chess;
 import junit.framework.TestCase;
 import org.junit.Test;
 import java.util.List;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class RookPieceTest
@@ -12,39 +15,85 @@ public class RookPieceTest
     public void getMoves00()
     {
         //given
-        RookPiece rook = new RookPiece(new Location(0,0), PieceColor.Black);
+        Location location = new Location(0,0);
+        RookPiece rook = new RookPiece(location, PieceColor.Black);
+        List<Move> validValues = new ArrayList<>();
+        for(int row = location.getRow()+1; row < 8; ++row)
+        {
+            validValues.add(new Move(location, new Location(row, location.getColumn()), false));
+        }
+        for(int col = location.getColumn()+1; col < 8; ++col)
+        {
+            validValues.add(new Move(location, new Location(location.getRow(), col), false));
+        }
         //when
         List<Move> list = rook.getMoves();
         //then
+        int count = 0;
+
         for(Move move: list)
         {
-            assertTrue(move.getTo().getColumn()==0 ^ move.getTo().getRow()==0);
+            assertEquals(move.getTo().getColumn(),validValues.get(count).getTo().getColumn());
+            assertEquals(move.getTo().getRow(),validValues.get(count).getTo().getRow());
+            count++;
         }
     }
     @Test
     public void getMoves88()
     {
         //given
+        Location location = new Location(7,7);
         RookPiece rook = new RookPiece(new Location(7,7), PieceColor.Black);
+        List<Move> validValues = new ArrayList<>();
+        for(int row = 0; row < 7; ++row)
+        {
+            validValues.add(new Move(location, new Location(row, location.getColumn()), false));
+        }
+        for(int col = 0; col < 7; ++col)
+        {
+            validValues.add(new Move(location, new Location(location.getRow(), col), false));
+        }
         //when
         List<Move> list = rook.getMoves();
         //then
+        int count = 0;
         for(Move move: list)
         {
-            assertTrue(move.getTo().getColumn()==7 ^ move.getTo().getRow()==7);
+            assertEquals(move.getTo().getColumn(),validValues.get(count).getTo().getColumn());
+            assertEquals(move.getTo().getRow(),validValues.get(count).getTo().getRow());
+            count++;
         }
     }
     @Test
     public void getMoves24()
     {
         //given
+        Location location = new Location(2,4);
         RookPiece rook = new RookPiece(new Location(2,4), PieceColor.Black);
+        List<Move> validValues = new ArrayList<>();
+        for(int row = 0; row < 8; ++row)
+        {
+            if (location.getRow() != row)
+            {
+                validValues.add(new Move(location, new Location(row, location.getColumn()), false));
+            }
+        }
+        for(int col = 0; col < 8; ++col)
+        {
+            if (location.getColumn() != col)
+            {
+                validValues.add(new Move(location, new Location(location.getRow(), col), false));
+            }
+        }
         //when
         List<Move> list = rook.getMoves();
         //then
+        int count = 0;
         for(Move move: list)
         {
-            assertTrue(move.getTo().getRow()==2 ^ move.getTo().getColumn()==4);
+            assertEquals(move.getTo().getColumn(),validValues.get(count).getTo().getColumn());
+            assertEquals(move.getTo().getRow(),validValues.get(count).getTo().getRow());
+            count++;
         }
     }
 }
