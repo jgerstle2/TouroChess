c3786625a14c5937e9df45dd63f016558d1805fe
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-09-30 12:06:20 -0400

    working test complete

--- src/test/java/touro/chess/KingPieceTest.java
+++ src/test/java/touro/chess/KingPieceTest.java
@@ -28,6 +28,21 @@ public class KingPieceTest {
         List<Move> legalMoves = king.getMoves();
 
         //then
-        assertEquals(practiceList,legalMoves);
+        assertEquals(legalMoves.get(0).getTo().getColumn(),practiceList.get(0).getTo().getColumn());
+        assertEquals(legalMoves.get(0).getTo().getRow(),practiceList.get(0).getTo().getRow());
+        assertEquals(legalMoves.get(1).getTo().getColumn(),practiceList.get(1).getTo().getColumn());
+        assertEquals(legalMoves.get(1).getTo().getRow(),practiceList.get(1).getTo().getRow());
+        assertEquals(legalMoves.get(2).getTo().getColumn(),practiceList.get(2).getTo().getColumn());
+        assertEquals(legalMoves.get(2).getTo().getRow(),practiceList.get(2).getTo().getRow());
+        assertEquals(legalMoves.get(3).getTo().getColumn(),practiceList.get(3).getTo().getColumn());
+        assertEquals(legalMoves.get(3).getTo().getRow(),practiceList.get(3).getTo().getRow());
+        assertEquals(legalMoves.get(4).getTo().getColumn(),practiceList.get(4).getTo().getColumn());
+        assertEquals(legalMoves.get(4).getTo().getRow(),practiceList.get(4).getTo().getRow());
+        assertEquals(legalMoves.get(5).getTo().getColumn(),practiceList.get(5).getTo().getColumn());
+        assertEquals(legalMoves.get(5).getTo().getRow(),practiceList.get(5).getTo().getRow());
+        assertEquals(legalMoves.get(6).getTo().getColumn(),practiceList.get(6).getTo().getColumn());
+        assertEquals(legalMoves.get(6).getTo().getRow(),practiceList.get(6).getTo().getRow());
+        assertEquals(legalMoves.get(7).getTo().getColumn(),practiceList.get(7).getTo().getColumn());
+        assertEquals(legalMoves.get(7).getTo().getRow(),practiceList.get(7).getTo().getRow());
     }
 }
