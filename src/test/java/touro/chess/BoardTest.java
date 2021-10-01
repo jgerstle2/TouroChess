bea9c661097dc7e5e4dc793ce772d239d3af0069
ElianaDG <58051386+ElianaDG@users.noreply.github.com>
2021-09-30 23:13:30 -0400

    finished testing

--- src/test/java/touro/chess/BoardTest.java
+++ src/test/java/touro/chess/BoardTest.java
@@ -27,25 +27,6 @@ public class BoardTest {
     }
 
     @Test
-    public void isLegal_invalidMoveSameColor(){
-        //given
-        Board board = new Board();
-        AbstractPiece piece = mock(AbstractPiece.class);
-        doReturn(PieceColor.White).when(piece.getColor());
-        Location currentLocation = new Location(2,1);
-        Location destinationLocation = new Location(4,1);
-        board.setPiece(currentLocation, piece);
-        board.setPiece(destinationLocation,piece);
-        Move move = new Move(currentLocation,destinationLocation,false);
-
-        //when
-        board.isLegal(move);
-
-        //then
-        Assert.assertTrue(board.isLegal(move));
-    }
-
-    @Test
     public void isLegal_validHorizontalMove(){
         //given
         Board board = new Board();
@@ -60,25 +41,6 @@ public class BoardTest {
 
         //then
         Assert.assertTrue(board.isLegal(move));
-    }
-
-    @Test
-    public void isLegal_invalidMoveBlockedPath(){
-        //given
-        Board board = new Board();
-        AbstractPiece piece = mock(AbstractPiece.class);
-        Location currentLocation = new Location(2,1);
-        Location destinationLocation = new Location(2,5);
-        Location middleLocation = new Location(2,3);
-        board.setPiece(currentLocation, piece);
-        board.setPiece(middleLocation,piece);
-        Move move = new Move(currentLocation,destinationLocation,false);
-
-        //when
-        board.isLegal(move);
-
-        //then
-        Assert.assertFalse(board.isLegal(move));
     }
 
     @Test
@@ -113,6 +75,44 @@ public class BoardTest {
 
         //then
         Assert.assertTrue(board.isLegal(move));
+    }
+
+    @Test
+    public void isLegal_invalidMoveSameColor(){
+        //given
+        Board board = new Board();
+        AbstractPiece piece = mock(AbstractPiece.class);
+        doReturn(PieceColor.White).when(piece).getColor();
+        Location currentLocation = new Location(2,1);
+        Location destinationLocation = new Location(4,1);
+        board.setPiece(currentLocation, piece);
+        board.setPiece(destinationLocation,piece);
+        Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
+
+        //then
+        Assert.assertFalse(board.isLegal(move));
+    }
+
+    @Test
+    public void isLegal_invalidMoveBlockedPath(){
+        //given
+        Board board = new Board();
+        AbstractPiece piece = mock(AbstractPiece.class);
+        Location currentLocation = new Location(2,1);
+        Location destinationLocation = new Location(2,5);
+        Location middleLocation = new Location(2,3);
+        board.setPiece(currentLocation, piece);
+        board.setPiece(middleLocation,piece);
+        Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
+
+        //then
+        Assert.assertFalse(board.isLegal(move));
     }
 
     @Test
