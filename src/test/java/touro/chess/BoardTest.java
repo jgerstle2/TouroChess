374dff6350476cef26433e90e4ea7e3a3924952f
ElianaDG <58051386+ElianaDG@users.noreply.github.com>
2021-09-30 21:59:08 -0400

    fixed tests

--- src/test/java/touro/chess/BoardTest.java
+++ src/test/java/touro/chess/BoardTest.java
@@ -3,13 +3,14 @@ package touro.chess;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 public class BoardTest {
 
 
     @Test
-    public void isLegal_validMove(){
+    public void isLegal_validVerticalMove(){
         //given
         Board board = new Board();
         AbstractPiece piece = mock(AbstractPiece.class);
@@ -25,130 +26,125 @@ public class BoardTest {
         Assert.assertTrue(board.isLegal(move));
     }
 
-//    @Test
-//    public void isLegal_invalidVerticalMove(){
-//        //given
-//        Location currentLocation = new Location(2,1);
-//        Location destinationLocation = new Location(4,1);
-//        Move move = new Move(currentLocation,destinationLocation,false);
-//
-//        //when
-//        board.isLegal(move);
-//
-//        //then
-//        Assert.assertFalse(board.isLegal(move));
-//    }
-//
-//    @Test
-//    public void isLegal_validHorizontalMove(){
-//        //given
-//        Location currentLocation = new Location(4,1);
-//        Location destinationLocation = new Location(4,6);
-//        Move move = new Move(currentLocation,destinationLocation,false);
-//
-//        //when
-//        board.isLegal(move);
-//
-//        //then
-//        Assert.assertTrue(board.isLegal(move));
-//    }
-//
-//    @Test
-//    public void isLegal_invalidHorizontalMove(){
-//        //given
-//        Location currentLocation = new Location(2,1);
-//        Location destinationLocation = new Location(2,5);
-//        Move move = new Move(currentLocation,destinationLocation,false);
-//
-//        //when
-//        board.isLegal(move);
-//
-//        //then
-//        Assert.assertFalse(board.isLegal(move));
-//    }
-//
-//    @Test
-//    public void isLegal_validDiagonalMove(){
-//        //given
-//        Location currentLocation = new Location(2,1);
-//        Location destinationLocation = new Location(4,3);
-//        Move move = new Move(currentLocation,destinationLocation,false);
-//
-//        //when
-//        board.isLegal(move);
-//
-//        //then
-//        Assert.assertTrue(board.isLegal(move));
-//    }
-//
-//    @Test
-//    public void isLegal_invalidDiagonalMove(){
-//        //given
-//        Location currentLocation = new Location(0,0);
-//        Location destinationLocation = new Location(7,7);
-//        Move move = new Move(currentLocation,destinationLocation,false);
-//
-//        //when
-//        board.isLegal(move);
-//
-//        //then
-//        Assert.assertFalse(board.isLegal(move));
-//    }
-//
-//    @Test
-//    public void isLegal_validJumpMove(){
-//        //given
-//        Location currentLocation = new Location(1,2);
-//        Location destinationLocation = new Location(3,1);
-//        Move move = new Move(currentLocation,destinationLocation,true);
-//
-//        //when
-//        board.isLegal(move);
-//
-//        //then
-//        Assert.assertTrue(board.isLegal(move));
-//    }
-//
-//    @Test
-//    public void isLegal_invalidJumpMove(){
-//        //given
-//        Location currentLocation = new Location(1,2);
-//        Location destinationLocation = new Location(2,4);
-//        Move move = new Move(currentLocation,destinationLocation,true);
-//
-//        //when
-//        board.isLegal(move);
-//
-//        //then
-//        Assert.assertFalse(board.isLegal(move));
-//    }
-//
-//    @Test
-//    public void isLegal_invalidMoveOffBoard(){
-//        //given
-//        Location currentLocation = new Location(12,1);
-//        Location destinationLocation = new Location(4,1);
-//        Move move = new Move(currentLocation,destinationLocation,false);
-//
-//        //when
-//        board.isLegal(move);
-//
-//        //then
-//        Assert.assertFalse(board.isLegal(move));
-//    }
-//
-//    @Test
-//    public void isLegal_invalidMoveSameSquare(){
-//        //given
-//        Location currentLocation = new Location(4,1);
-//        Location destinationLocation = new Location(4,1);
-//        Move move = new Move(currentLocation,destinationLocation,false);
-//
-//        //when
-//        board.isLegal(move);
-//
-//        //then
-//        Assert.assertFalse(board.isLegal(move));
-//    }
+    @Test
+    public void isLegal_invalidMoveSameColor(){
+        //given
+        Board board = new Board();
+        AbstractPiece piece = mock(AbstractPiece.class);
+        doReturn(PieceColor.White).when(piece.getColor());
+        Location currentLocation = new Location(2,1);
+        Location destinationLocation = new Location(4,1);
+        board.setPiece(currentLocation, piece);
+        board.setPiece(destinationLocation,piece);
+        Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
+
+        //then
+        Assert.assertTrue(board.isLegal(move));
+    }
+
+    @Test
+    public void isLegal_validHorizontalMove(){
+        //given
+        Board board = new Board();
+        AbstractPiece piece = mock(AbstractPiece.class);
+        Location currentLocation = new Location(4,1);
+        Location destinationLocation = new Location(4,6);
+        board.setPiece(currentLocation, piece);
+        Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
+
+        //then
+        Assert.assertTrue(board.isLegal(move));
+    }
+
+    @Test
+    public void isLegal_invalidMoveBlockedPath(){
+        //given
+        Board board = new Board();
+        AbstractPiece piece = mock(AbstractPiece.class);
+        Location currentLocation = new Location(2,1);
+        Location destinationLocation = new Location(2,5);
+        Location middleLocation = new Location(2,3);
+        board.setPiece(currentLocation, piece);
+        board.setPiece(middleLocation,piece);
+        Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
+
+        //then
+        Assert.assertFalse(board.isLegal(move));
+    }
+
+    @Test
+    public void isLegal_validDiagonalMove(){
+        //given
+        Board board = new Board();
+        AbstractPiece piece = mock(AbstractPiece.class);
+        Location currentLocation = new Location(2,1);
+        Location destinationLocation = new Location(4,3);
+        board.setPiece(currentLocation, piece);
+        Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
+
+        //then
+        Assert.assertTrue(board.isLegal(move));
+    }
+
+    @Test
+    public void isLegal_validJumpMove(){
+        //given
+        Board board = new Board();
+        AbstractPiece piece = mock(AbstractPiece.class);
+        Location currentLocation = new Location(1, 2);
+        Location destinationLocation = new Location(3,1);
+        board.setPiece(currentLocation, piece);
+        Move move = new Move(currentLocation,destinationLocation,true);
+
+        //when
+        board.isLegal(move);
+
+        //then
+        Assert.assertTrue(board.isLegal(move));
+    }
+
+    @Test
+    public void isLegal_invalidMoveOffBoard(){
+        //given
+        Board board = new Board();
+        Location currentLocation = new Location(12,1);
+        Location destinationLocation = new Location(4,1);
+        Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
+
+        //then
+        Assert.assertFalse(board.isLegal(move));
+    }
+
+    @Test
+    public void isLegal_invalidMoveSameSquare(){
+        //given
+        Board board = new Board();
+        AbstractPiece piece = mock(AbstractPiece.class);
+        Location currentLocation = new Location(4,1);
+        Location destinationLocation = new Location(4,1);
+        board.setPiece(currentLocation, piece);
+        Move move = new Move(currentLocation,destinationLocation,false);
+
+        //when
+        board.isLegal(move);
+
+        //then
+        Assert.assertFalse(board.isLegal(move));
+    }
 
 }
