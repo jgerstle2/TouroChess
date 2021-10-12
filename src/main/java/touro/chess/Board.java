21bac62a0c9758388f289027ac082b3967ae4c10
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-10-12 11:57:52 -0400

    pieces in between the castling

--- src/main/java/touro/chess/Board.java
+++ src/main/java/touro/chess/Board.java
@@ -137,12 +137,9 @@ public class Board {
                 //no pieces in between king and rook
                 Square right1 = getSquare(new Location(currentRow, currentColumn + 1));
                 Square right2 = getSquare(new Location(currentRow, currentColumn + 2));
-                if (right1.hasPiece() && right2.hasPiece()){
-                    return false;
-                }
                 Square left1 = getSquare(new Location(currentRow, currentColumn - 1));
                 Square left2 = getSquare(new Location(currentRow, currentColumn - 2));
-                if (left1.hasPiece() && left2.hasPiece()){
+                if ((right1.hasPiece() && right2.hasPiece()) || (left1.hasPiece() && left2.hasPiece())){
                     return false;
                 }
                 //not moving through check
